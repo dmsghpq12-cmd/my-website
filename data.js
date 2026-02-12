b4c03c544ae27ff07ed42b07871b2c4bacbc02b5
dmsghpq12-cmd <dmsghpq12@gmail.com>
2026-02-12 15:59:45 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,45 @@
+// ===== 공통 제품 데이터 =====
+// 목록(products) + 상세(detail) 공통 사용
+
+window.DATA = {
+  marker: [
+    {
+      id: 1,
+      name: "Test",
+      color: "Black",
+      size: "10mm",
+      date: "2026-02-12",
+      img: "images/marker-001.jpg"
+    },
+    {
+      id: 2,
+      name: "Test2",
+      color: "Red",
+      size: "12mm",
+      date: "2026-02-12",
+      img: "images/marker-002.jpg"
+    }
+  ],
+
+  cttape: [
+    {
+      id: 1,
+      name: "Test3",
+      color: "Transparent",
+      size: "10mm x 50m",
+      date: "2026-02-12",
+      img: "images/ct-001.jpg"
+    }
+  ],
+
+  material: [
+    {
+      id: 1,
+      name: "Test4",
+      color: "White",
+      size: "Sheet",
+      date: "2026-02-12",
+      img: "images/material-001.jpg"
+    }
+  ]
+};
